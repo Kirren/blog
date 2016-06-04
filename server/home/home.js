5ebc0632de72ec16eab30118a1093b3e0d7ad584
pavlovsch <a.pavlov.sch@gmail.com>
2016-06-04 19:39:27 +0500

    'upd'

--- server/home/home.js
+++ server/home/home.js
@@ -12,5 +12,3 @@ router.get('/', (req, res) => {
     })
   })
 })
-
-// INSERT INTO posts (id, title, date, action, text, hashtags, gallery) VALUES ('4', 'Everythin in the universe has a rhytm, everything dances', 'Sep 24', 'I Do Photos', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae laboriosam incidunt itaque consectetur modi, eos ipsa, totam accusamus dicta dolorum ipsam sunt dolores reiciendis saepe! Nam iure, quos voluptatibus distinctio.', '#Yosemite #Photo #Hi-res', '["//static.pexels.com/photos/24641/pexels-photo-24641-large.jpg", "//static.pexels.com/photos/96491/pexels-photo-96491-large.jpeg", "//static.pexels.com/photos/5129/person-mountain-hiker-hiking-large.jpg", "//static.pexels.com/photos/27111/pexels-photo-27111-large.jpg"]')
