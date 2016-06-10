183d356c7b19f3969dc96ffcd63c934ffbb608c6
pavlovsch <a.pavlov.sch@gmail.com>
2016-06-10 14:42:51 +0500

    upd info

--- server/config.js
+++ server/config.js
@@ -1,10 +1,28 @@
 import mysql from 'mysql'
 
+
+// SET CONNECTION TO YOUR DATABASE WITH YOUR CONFIG
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'kowa4ogg',
-  database: 'blog'
+  host: 'host',
+  user: 'user',
+  password: 'password',
+  database: 'database'
 })
+
+/*
+YOU NEED TO CREATE TABLE 'posts' WITH COLUMNS:
+
+id TINYTEXT
+title TINYTEXT
+text TEXT
+action TINYTEXT
+date TINYTEXT
+video TINYTEXT
+quote TINYTEXT
+picture TINYTEXT
+hashtags TINYTEXT
+gallery JSON
+
+*/
 
 export const PORT = process.env.PORT || 8080
