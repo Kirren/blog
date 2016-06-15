8322e1d6944d938c7d26d819165db7e139e55840
pavlovsch <a.pavlov.sch@gmail.com>
2016-06-15 17:38:31 +0500

    nvm

--- server/pages/contact/contact.js
+++ server/pages/contact/contact.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const nodemailer = require('nodemailer')
+const mg = require('nodemailer-mailgun-transport')
 
 router.get('/', (req, res) => {
   res.render('contact')
@@ -10,8 +11,8 @@ router.post('/sendletter', (req, res) => {
   const email = req.body.email
   const subject = req.body.subject
   const text = req.body.message
-
-  // make it work
+  res.redirect('back')
+  // to be continued.
 })
 
 module.exports.router = router
